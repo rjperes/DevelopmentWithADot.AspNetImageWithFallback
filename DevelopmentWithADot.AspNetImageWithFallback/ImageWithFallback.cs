0aa9ddc61a3b52dba0413c6c1dd94cb6aa08571b
Ricardo Peres <rjperes@hotmail.com>
2013-06-18 17:02:08 +0100

    Initial version

---
+++ DevelopmentWithADot.AspNetImageWithFallback/ImageWithFallback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevelopmentWithADot.AspNetImageWithFallback
+{
+	public class ImageWithFallback : Image
+	{
+		[DefaultValue("")]
+		[UrlProperty]
+		public String FallbackUrl
+		{
+			get;
+			set;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			if (String.IsNullOrWhiteSpace(this.FallbackUrl) == false)
+			{
+				this.Attributes["onerror"] = String.Format("this.src = '{0}'", this.ResolveUrl(this.FallbackUrl));
+			}
+
+			base.OnLoad(e);
+		}
+	}
+}
